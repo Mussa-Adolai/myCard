d7dbf0adaae4b21d2bc5b3e2f81b73165f1ec077
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-07-06 00:09:28 +0300

    my card 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,12 +13,41 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-           
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                backgroundColor: Colors.blue,
                 radius: 50.0,
-              )
+                backgroundImage: AssetImage('images/my_card.jpg'),
+              ),
+              Text(
+                'mussa adolai',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 3.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Container(
+                color: Colors.grey,
+                height: 1.0,
+                width: 400.0,
+                margin: EdgeInsets.symmetric(horizontal: 130.0, vertical: 20.0),
+              ),
+              Container(
+                width: double.infinity,
+              ),
             ],
           ),
         ),
